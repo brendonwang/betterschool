7de5a34959c6e2767cf01e6f6f213ec5e2614a26
Glitch (jasper-inexpensive-locust) <none>
2022-08-27 02:47:51 +0000

    😼🌓 Checkpoint ./science/game1.html:48059908/437:48061182/123 ./science/game.js:48059908/1902:48061182/2349 ./css/lessons.css:48059908/137

--- science/game.js
+++ science/game.js
@@ -1,27 +1,27 @@
-// function shuffle(array) {
-//   let currentIndex = array.length;
-//   let randomIndex = 0;
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex = 0;
 
-//   // While there remain elements to shuffle.
-//   while (currentIndex != 0) {
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
 
-//     // Pick a remaining element.
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex--;
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-//     // And swap it with the current element.
-//     [array[currentIndex], array[randomIndex]] = [
-//       array[randomIndex], array[currentIndex]];
-//   }
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
 
-//   return array;
-// }
+  return array;
+}
 
 const question = document.getElementById("question");
 const b1 = document.getElementById("b1");
-// const b2 = document.getElementById("b2");
-// const b3 = document.getElementById("b3");
-// const b4 = document.getElementById("b4");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
 
 const questions = ["A", "B", "C"];
 const answer_choices = {
@@ -32,16 +32,28 @@ const answer_choices = {
 const correct_answer = { A: "A", B: "B", C: "C" };
 let click = false;
 let answer = "";
-// shuffle(questions);
+shuffle(questions);
 for (let i = 0; i < questions.length; ++i) {
   question.innerText = questions[i];
-  // shuffle(answer_choices[i]);
+  shuffle(answer_choices[i]);
   for (let j = 0; j < 4; ++j){
-    document.getElementById("b"+j).innerText = answer_choices[i];
+    document.getElementById("b"+j).innerText = answer_choices[i][j];
   }
   while(!click){}
 }
 function b1click(){
   click = true;
   answer = b1.innerText;
+}
+function b2click(){
+  click = true;
+  answer = b2.innerText;
+}
+function b3click(){
+  click = true;
+  answer = b3.innerText;
+}
+function b4click(){
+  click = true;
+  answer = b4.innerText;
 }
