c65bcdb7a552ac310bf16b8166b4103515f4b0da
brendon.wang <brendon.wang@gmail.com>
2022-08-27 07:27:26 -0700

    Delete unesseary stuff

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("webdictionary.txt"));
-fclose($myfile);
-?>
-<?php 
-$file = fopen("login.txt", "r");
-echo $_GET["uname"]; ?>
