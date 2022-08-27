42f10fbd319bd2beca049cea61116d1a74cab7da
brendon.wang <brendon.wang@gmail.com>
2022-08-27 08:48:17 -0700

    Make Coin System Work

--- coins.js
+++ coins.js
@@ -1,40 +1,11 @@
 let coins = document.getElementById("coins");
-
-function getText(){
-    // read text from URL location
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://betterschool.glitch.me/coins.txt', true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                return request.responseText;
-            }
-        }
-    }
-}
-function getText2(){
-    // read text from URL location
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://betterschool.glitch.me/username.txt', true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                return request.responseText;
-            }
-        }
-    }
-}
 let arr = []
-fetch("https://betterschool.glitch.me/coins.txt").then((r)=>{r.text().then((d)=>{arr = (d.split('\n')).split(' ')})})
-
+fetch("https://betterschool.glitch.me/coins.txt").then((r)=>{r.text().then((d)=>{arr = (d.split('\n'))})})
 let username = "";
 fetch("https://betterschool.glitch.me/username.txt").then((r)=>{r.text().then((d)=>{username = d})})
 
 for (let i = 0; i < arr.length; ++i){
+  arr[i].split(' ')
   if (arr[i][0] == username){
     coins.innerText = arr[i][1]
   }
--- science/game.js
+++ science/game.js
@@ -1,22 +1,7 @@
 function shuffle(array) {
-  let currentIndex = array.length;
-  let randomIndex = 0;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
+  array.sort(() => Math.random() - 0.5);
 }
+
 const question = document.getElementById("question");
 const b1 = document.getElementById("b1");
 const b2 = document.getElementById("b2");
