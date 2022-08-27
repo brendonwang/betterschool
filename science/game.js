6343d7e8877c05ffb302a4eb5d93dd7da4ad6b1a
Glitch (jasper-inexpensive-locust) <none>
2022-08-27 02:35:53 +0000

    🚛🕰 Checkpoint ./science/game.js:48059908/1460:48061182/2 ./science/game1.html:48059908/127

--- science/game.js
+++ science/game.js
@@ -1,12 +1,28 @@
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
+// function shuffle(array) {
+//   let currentIndex = array.length;
+//   let randomIndex = 0;
+
+//   // While there remain elements to shuffle.
+//   while (currentIndex != 0) {
+
+//     // Pick a remaining element.
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
+
+//     // And swap it with the current element.
+//     [array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex], array[currentIndex]];
+//   }
+
+//   return array;
+// }
+
 const question = document.getElementById("question");
+const b1 = document.getElementById("b1");
+// const b2 = document.getElementById("b2");
+// const b3 = document.getElementById("b3");
+// const b4 = document.getElementById("b4");
+
 const questions = ["A", "B", "C"];
 const answer_choices = {
   A: ["A", "B", "C", "D"],
@@ -14,8 +30,18 @@ const answer_choices = {
   C: ["A", "B", "C", "D"],
 };
 const correct_answer = { A: "A", B: "B", C: "C" };
-shuffleArray(questions);
+let click = false;
+let answer = "";
+// shuffle(questions);
 for (let i = 0; i < questions.length; ++i) {
   question.innerText = questions[i];
-  setTimeout(1000);
+  // shuffle(answer_choices[i]);
+  for (let j = 0; j < 4; ++j){
+    document.getElementById("b"+j).innerText = answer_choices[i];
+  }
+  while(!click){}
+}
+function b1click(){
+  click = true;
+  answer = b1.innerText;
 }
