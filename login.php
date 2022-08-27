5f0ceb9474bf0c12bfe5ec5e0e50af05bd2caf29
Glitch (jasper-inexpensive-locust) <none>
2022-08-27 04:45:55 +0000

    🚠🏫 Checkpoint ./login.html:48059908/21924 ./login.php:48059908/440 ./css/login.css:48059908/7569 ./login.js:48059908/7757 ./css/index.css:48059908/14590

--- login.php
+++ login.php
@@ -3,3 +3,6 @@ $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("webdictionary.txt"));
 fclose($myfile);
 ?>
+<?php 
+$file = fopen("login.txt", "r");
+echo $_GET["uname"]; ?>
