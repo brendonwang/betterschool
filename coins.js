78b54353cf740fdab6619c5c04bfdc959b9d6df7
Glitch (betterschool) <none>
2022-08-27 19:28:05 +0000

    📩🦨 Updated with Glitch

--- coins.js
+++ coins.js
@@ -4,22 +4,57 @@ async function fetchFile(file) {
   const exam = await response.text();
   return exam;
 }
-let arr = [];
-fetchFile("/coins.txt").then(value => {
-  value;
-  console.log(value[5]);
-  let username = fetchFile("/username.txt").then(result => {
-    for(let i = 0; i < value.length; ++i){
-      arr[i].split();
-      if (arr[i][0] == result){
-        coin.innerText = arr[i][1];
-        break;
-      }
-    }
-  })
-  ;});
-// let username = fetchFile("/username.txt").then(result => {});
+// fetchFile("/coins.json").then((value) => {
+//   let arr = [];
+//   let prev = "";
+//   for (const e of value) {
+//     if (e == "\n") {
+//       arr.push(prev);
+//       prev = "";
+//     } else {
+//       prev += e;
+//     }
+//   }
+//   arr.push(prev);
+//   let username = fetchFile("/username.txt").then((result) => {
+//     for (let i = 0; i < value.length; ++i) {
+//       let arr2 = [];
+//       prev = "";
+//       for (const e of arr[i]) {
+//         if (e == ' ') {
+//           arr2.push(prev);
+//           prev = "";
+//         } else {
+//           prev += e;
+//         }
+//       }
+//       arr2.push(prev);
+//       if (arr2[0] == result) {
+//         coin.innerText = arr2[1];
+//         break;
+//       }
+//     }
+//   });
+// });
+require(["fs"], function (fs) {
+  //foo is now loaded.
+    fetchFile("/coins.json").then((value) => {
+      value = JSON.parse(value);
+      fetchFile("/username.txt").then((result) => {
+        if (value[result] == undefined) {
+          value[result] = 0;
+          value = JSON.stringify(value);
+          fs.writeFile("myjsonfile.json", value, "utf8");
+        } else {
+          coin.innerText = value[result];
+        }
+        console.log(value);
+        console.log(value[result]);
+      });
+    });
+});
 
+// let username = fetchFile("/username.txt").then(result => {});
 
 // for (let i = 0; i < arr.length; ++i) {
 //   arr[i].split(" ");
